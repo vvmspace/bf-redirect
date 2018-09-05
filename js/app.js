920651864723471d8eb90354ddf1f6ae29216ca1
vvmsecured <vvmspace@gmail.com>
2018-09-06 01:16:43 +0500

    base app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 const BF = new Vue({
     el: '#BF',
-    created: () => {
-
+    data: {
+        countdown: 5,
+        redirect_url: 'https://www.e1.ru/award/votes/fitness/'
+    },
+    created() {
+        this.initTicker();
+    },
+    methods: {
+        initTicker: function() {
+            let self = this;
+            setInterval(() => {self.tick();}, 1000);
+        },
+        tick: function() {
+            if(this.countdown >= 0){
+                this.countdown--;
+            }else{
+                document.location.href = this.redirect_url;
+            }
+        }
     }
 });
